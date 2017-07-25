44a6d6c8bd60b8d79caa097f2a385ada4a5cd997
FrOnDaL <frondalbolu@gmail.com>
2017-07-25 17:40:34 +0300

    Update

---
+++ FrOnDaL Thresh/FrOnDaL Thresh/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Aimtec;
+using Aimtec.SDK.Events;
+
+namespace FrOnDaL_Thresh
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            GameEvents.GameStart += OnLoadingComplete;
+        }
+        private static void OnLoadingComplete()
+        {
+            if (ObjectManager.GetLocalPlayer().ChampionName != "Thresh") return;
+            var unused = new FrOnDaLThresh();
+            Console.WriteLine("FrOnDaL Thresh loaded");
+        }
+    }
+}
